8ab8016acc22050047bfb10ada630532ef9178d5
kernoeb <kernoeb@protonmail.com>
2021-03-14 01:17:05 +0100

    fix: use POST/PUT to avoid random color

--- app.py
+++ app.py
@@ -23,26 +23,32 @@ with open('leds.json') as json_file:
         leds.append(Led(d['mac'], d['uuid'], d['address']))
 
 
-@app.route('/colors', methods=['GET', 'POST'])
+@app.route('/colors', methods=['GET', 'POST', 'PUT'])
 def color():
     if request.method == 'GET':
         tmp = []
         for led in leds:
             tmp.append({'mac': led.address, 'color': led.currentColor, 'brightness': led.currentBrightness})
         return jsonify(tmp)
-    elif request.method == 'POST':
+    elif request.method == 'POST' or request.method == 'PUT':
         try:
             req = request.get_json(silent=True)
+
+            tmp_color = None
             if req is not None and req.get('color') is not None and req.get('color').startswith('#') and len(
                     req.get('color')) == 7:
                 tmp_color = req.get('color')
-            else:
+            elif request.method == 'PUT':
                 tmp_color = random_color()
+
             if req is not None and req.get('brightness') is not None and 0 <= int(req.get('brightness')) <= 100:
                 for led in leds:
                     led.set_brightness(int(req.get('brightness')))
-            for led in leds:
-                led.set_color(tmp_color)
+
+            if tmp_color is not None:
+                for led in leds:
+                    led.set_color(tmp_color)
+
             return jsonify({'status': 'ok'}), 200
         except Exception as e:
             print(e)
